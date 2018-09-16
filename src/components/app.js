3173f1b6d555c996b49e51bc9a53b798c611675b
Esther <esther2180@gmail.com>
2018-09-15 22:57:33 -0700

    React-Clock Prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,62 @@
+// import React from 'react';
+// import Table from './table';
+
+// export default () => {
+
+//     const students = [
+//         {
+//             name: "Arden Kona",
+//             course: "Drawing",
+//             grade: 89
+//         },
+//         {
+//             name: "Sam Parker",
+//             course: "Math",
+//             grade: 99
+//         },
+//         {
+//             name: "Elisha Kimberly",
+//             course: "English",
+//             grade: 98
+//         },
+//     ];
+
+//     return (
+//         <div className="container">
+//             <h1>"Student Grade Table"</h1>
+//             <Table data={students} />
+//         </div>
+//     )
+// }
+
 import React from 'react';
-import { lstat } from 'fs';
+import Table from './table';
 
 export default () => {
-    return <h1>"My first React functional component"</h1>
+
+    const students = [
+        {
+            name: 'Peter Parker',
+            course: 'Intro to Phtography',
+            grade: 85 
+        },
+        {
+            name: 'Bruce Banner',
+            course: 'Psychology',
+            grade: 91
+        },
+        {
+            name: 'Tony Stark',
+            course: 'Mechanical Engineering',
+            grade: 100    
+        },
+    ];
+
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )    
 }
+
