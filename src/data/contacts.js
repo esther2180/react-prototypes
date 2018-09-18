a66fc845eb0ffc1bdd6da592a37568e4d49bc3a6
Esther <esther2180@gmail.com>
2018-09-17 23:34:33 -0700

    React - Form Inputs

---
+++ src/data/contacts.js
@@ -0,0 +1,20 @@
+export default [
+    {
+    firstName: 'Bruno',
+    lastName: 'Mars',
+    phone: '555-555-5555',
+    email: 'emailMe5@email.com'
+    },
+    {
+        firstName: 'Cardi',
+        lastName: 'B',
+        phone: '777-777-7777',
+        email: 'emailMe7@email.com'
+    },
+    {
+        firstName: 'Jordan',
+        lastName: 'Sparks',
+        phone: '999-999-9999',
+        email: 'emailMe9@email.com'
+    },
+];
