6605490b3248a9b946aafddd03c310d52dc4ca9f
Esther <esther2180@gmail.com>
2018-09-23 08:27:13 -0700

    React Routing with React Router v4

---
+++ src/components/gifts_parties.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1>Gifts &amp; Parties</h1>
+        <p>Bacon ipsum dolor amet venison short ribs ham meatball boudin prosciutto. Ground round bacon filet mignon, venison tail hamburger shoulder ribeye swine andouille turkey kielbasa chuck ham spare ribs. Prosciutto turkey pork belly shankle, drumstick short loin ribeye filet mignon pig flank sausage frankfurter buffalo kevin.</p>
+    </div>
+)
